853feb57c6f3fb53f2f099d66fcdde8ad33da706
neeld <neeldc2@gmail.com>
2025-03-29 13:25:07 +0530

    first commit

---
+++ src/main/java/com/neel/security_no_gateway_testing/SecurityNoGatewayTestingApplication.java
@@ -0,0 +1,13 @@
+package com.neel.security_no_gateway_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityNoGatewayTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityNoGatewayTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neel/security_no_gateway_testing/SecurityNoGatewayTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neel.security_no_gateway_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityNoGatewayTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
